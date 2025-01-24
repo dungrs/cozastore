3038baee593655b2a32aeab5cd5ad468b5c04829
dungrs <dungnhung1209@gmail.com>
2025-01-24 19:52:32 +0700

    Start project

---
+++ resources/views/backend/component/head.blade.php
@@ -0,0 +1,21 @@
+<meta charset="utf-8" />
+<title>Vertical | Vuesy - Admin & Dashboard Template</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+<meta content="Themesdesign" name="author" />
+<link rel="shortcut icon" href="{{ asset('backend/images/favicon.ico') }}">
+
+<!-- plugin css -->
+<link href="{{ asset('backend/libs/jsvectormap/css/jsvectormap.min.css') }}" rel="stylesheet" type="text/css" />
+
+<!-- swiper css -->
+<link rel="stylesheet" href="{{ asset('backend/libs/swiper/swiper-bundle.min.css') }}">
+
+<!-- Bootstrap Css -->
+<link href="{{ asset('backend/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+
+<!-- Icons Css -->
+<link href="{{ asset('backend/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+<!-- App Css-->
+<link href="{{ asset('backend/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
---
+++ resources/views/backend/component/script.blade.php
@@ -0,0 +1,18 @@
+<script src="{{ asset('backend/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('backend/libs/metismenujs/metismenujs.min.js') }}"></script>
+<script src="{{ asset('backend/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('backend/libs/feather-icons/feather.min.js') }}"></script>
+
+<!-- apexcharts -->
+<script src="{{ asset('backend/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+<!-- Vector map-->
+<script src="{{ asset('backend/libs/jsvectormap/js/jsvectormap.min.js') }}"></script>
+<script src="{{ asset('backend/libs/jsvectormap/maps/world-merc.js') }}"></script>
+
+<!-- swiper js -->
+<script src="{{ asset('backend/libs/swiper/swiper-bundle.min.js') }}"></script>
+
+<script src="{{ asset('backend/js/pages/dashboard.init.js') }}"></script>
+
+<script src="{{ asset('backend/js/app.js') }}"></script>
